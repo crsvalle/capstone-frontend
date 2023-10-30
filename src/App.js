49b045d4a25b8e4c2e593f4d16110d5f3dff764b
crsvalle <cristian.valle@pursuit.org>
2023-10-30 19:31:13 -0400

    inital code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+//DEPENDENCIES
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+//PAGES
+import Home from "./Pages/Home";
+
+//COMPONENTS
+import Navbar from "./Components/Navbar";
+
+//STYLE
 import './App.css';
 
 function App() {
-  return;
+  return (
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
