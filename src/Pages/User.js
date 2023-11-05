bc62a3a8461afdab9e49dce62b130d08140e0070
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-05 11:15:05 -0500

    user profile created

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -1,9 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 export default function User() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API}/users/${id}`)
+    .then((res) => setUser(res.data))
+    .catch((e) => console.warn("catch", e))
+  }, [])
+
   return (
-    //ADD USER LISTINGS
-    //ADD USER LISTING REVIEWS
-    //ADD USER RATINGS
-    <div>User</div>
+    <>
+    <div class="conainer">
+        <img src="" alt="avatar"/>
+        <div class="profile">
+            <span>****</span>
+            <h1>{user.firstName + " " + user.lastName}</h1>
+            <p>role :{user.role}</p>
+            <p>add :{user.address}</p>
+            <p>phone :{user.phone}</p>
+            <p>email :{user.email}</p>
+            <p>password :{user.password}</p>
+            <p>verified :{user.verified}</p>
+            <p>member since :{user.created_at}</p> 
+            <hr/>
+            <p>payment info</p>
+        </div>
+        <div class="message">message</div>
+    </div>
+    <div class="reviews">reviews for</div>
+    <div class="reviews__posted">reviews posted</div>
+    </>
   )
 }
