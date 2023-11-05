c2a1cfa100af217b0e0edbe5e292157730f94408
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-05 15:15:45 -0500

    added basic navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,15 @@
-import '../Style/Navbar.css'
+import '../style/Navbar.css';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className='nav'>
+      <img src='' alt='logo' className='nav__logo' />
+      <div className='nav__end'>
+        <div className='nav__item'>About</div>
+        <div className='nav__item'>Login</div>
+        <div>/</div>
+        <div className='nav__item'>Register</div>
+      </div>
+    </div>
+  );
 }
