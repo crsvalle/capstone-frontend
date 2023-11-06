0bbef5b587ce76af940000b61eb3217f59975af2
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-05 21:17:09 -0500

    added hero pic png and added classname search__bar to search bar component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import Searchbar from "../Components/Searchbar";
 import Lists from "../Components/Lists"
 import '../style/Home.css'
+import hero from './house.png'
 
 
 export default function Home() {
   return (
     <div className="home">
-      <Searchbar/>
+      <img src={hero}></img>
+      <Searchbar className="search__bar"/>
       <Lists/>
     </div>
   )
