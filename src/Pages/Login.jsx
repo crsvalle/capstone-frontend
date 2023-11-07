6dc94b287780c0ff6b1e0fd025fcbcd38ba244de
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-06 19:49:43 -0500

    added required attribute to each field

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,27 +7,27 @@ export default function Login () {
                 <table>
                     <tr>
                         <td>First Name:</td>
-                        <td><input type="text" name="fullname" id="fullname" /></td>
+                        <td><input type="text" name="fullname" id="fullname" required /></td>
                     </tr>
                     <tr>
                         <td>Last Name:</td>
-                        <td><input type="text" name="fullname" id="fullname" /></td>
+                        <td><input type="text" name="fullname" id="fullname" required /></td>
                     </tr>
                     <tr>
                         <td>Email:</td>
-                        <td><input type="email" name="email" id="email" /></td>
+                        <td><input type="email" name="email" id="email" required/></td>
                     </tr>
                     <tr>
                         <td>Confirm Email:</td>
-                        <td><input type="email" name="confirmEmail" id="confirmEmail" /></td>
+                        <td><input type="email" name="confirmEmail" id="confirmEmail" required/></td>
                     </tr>
                     <tr>
                         <td>Password:</td>
-                        <td><input type="password" name="password" id="password" /></td>
+                        <td><input type="password" name="password" id="password" required/></td>
                     </tr>
                     <tr>
                         <td>Confirm Password:</td>
-                        <td><input type="password" name="confirmPassword" id="confirmPassword" /></td>
+                        <td><input type="password" name="confirmPassword" id="confirmPassword" required/></td>
                     </tr>
                 </table>
                 <input type="submit" value="Submit" />
