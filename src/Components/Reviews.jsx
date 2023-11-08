8276a16b713d202b54cecd66c3d9ba7752e769df
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-07 21:47:33 -0500

    added star rating

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,11 +1,12 @@
 import '../style/review.css'
+import Ratings from './Ratings'
 
 export default function Reviews({review}) {
     return (
         <div className='review__container'>
             <div className='review__container__profile__pic'>
-                <img src="" alt="pic" />
-                <div>{review.rating}</div>
+                <img src={review.image} alt="pic" />
+                <Ratings rating={review.rating}/>
             </div>
             <div>{review.title}</div>
             <div>{review.body}</div>
