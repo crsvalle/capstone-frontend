6b0809ad858c61a28cb63ebe09808d80fa4d767a
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-09 10:31:52 -0500

    created auth login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,38 +1,44 @@
-import "../style/Login.css"
+import '../style/Login.css'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
-export default function Login () {
-    return (
-        <div className="form__container">
-            <form>
-                <table>
-                    <tr>
-                        <td>First Name:</td>
-                        <td><input type="text" name="fullname" id="fullname" required /></td>
-                    </tr>
-                    <tr>
-                        <td>Last Name:</td>
-                        <td><input type="text" name="fullname" id="fullname" required /></td>
-                    </tr>
-                    <tr>
-                        <td>Email:</td>
-                        <td><input type="email" name="email" id="email" required/></td>
-                    </tr>
-                    <tr>
-                        <td>Confirm Email:</td>
-                        <td><input type="email" name="confirmEmail" id="confirmEmail" required/></td>
-                    </tr>
-                    <tr>
-                        <td>Password:</td>
-                        <td><input type="password" name="password" id="password" required/></td>
-                    </tr>
-                    <tr>
-                        <td>Confirm Password:</td>
-                        <td><input type="password" name="confirmPassword" id="confirmPassword" required/></td>
-                    </tr>
-                </table>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-        
-    )
-};
+export default function Login() {
+  return (
+    <Box className='login__box'
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        '& button': { m: 1 },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div className='login__box__main'> 
+        <TextField
+          required
+          id="outlined-input"
+          label="Email"
+          defaultValue=""
+        />
+        <TextField
+          required  
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+      <div className='login__box__btn'>
+        <Button variant="outlined" size="large">
+            LOGIN
+        </Button>
+      </div>
+      <div className="login__box__footer">
+      <div className="forgetPass">Forgot Password?</div>
+      <div className='signUp'>Sign up Here!</div>
+      </div>
+      </div>
+    </Box>
+  );
+}
