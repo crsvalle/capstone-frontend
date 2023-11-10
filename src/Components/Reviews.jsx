a94474cadb1ee558277cdefe5f21331709bfe284
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-09 19:52:51 -0500

    changed img tag for review component to avatar from mui

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,17 +1,17 @@
-import '../style/review.css'
-import Ratings from './Ratings'
+import '../style/review.css';
+import Ratings from './Ratings';
+import Avatar from '@mui/material/Avatar';
 
 export default function Reviews({review}) {
     return (
         <div className='review__container'>
             <div className='review__container__profile__pic'>
-                <img src={review.image} alt="pic" />
+                <Avatar src='' alt='pic' sx={{ width: 30, height: 30 }} />
                 <Ratings rating={review.rating}/>
             </div>
             <div>{review.title}</div>
             <div>{review.body}</div>
             <hr className='review__hr'/>
         </div>
-
     )
 }
