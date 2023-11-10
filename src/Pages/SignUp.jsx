fc0e351412cf31f52cf9b9cdbd54d2780274f3f3
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-09 19:39:41 -0500

    added classname form__input so as not to conflict with the styling of login page

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -7,27 +7,63 @@ export default function SignUp () {
                 <table>
                     <tr>
                         <td>First Name:</td>
-                        <td><input type="text" name="fullname" id="fullname" required /></td>
+                        <td><input 
+                            className="form__input"
+                            type="text" 
+                            name="fullname" 
+                            id="fullname" 
+                            required 
+                        /></td>
                     </tr>
                     <tr>
                         <td>Last Name:</td>
-                        <td><input type="text" name="fullname" id="fullname" required /></td>
+                        <td><input 
+                            className="form__input"
+                            type="text"
+                            name="fullname"
+                            id="fullname"
+                            required
+                         /></td>
                     </tr>
                     <tr>
                         <td>Email:</td>
-                        <td><input type="email" name="email" id="email" required/></td>
+                        <td><input 
+                            className="form__input"
+                            type="email" 
+                            name="email" 
+                            id="email" 
+                            required
+                        /></td>
                     </tr>
                     <tr>
                         <td>Confirm Email:</td>
-                        <td><input type="email" name="confirmEmail" id="confirmEmail" required/></td>
+                        <td><input 
+                            className="form__input"
+                            type="email" 
+                            name="confirmEmail" 
+                            id="confirmEmail" 
+                            required
+                        /></td>
                     </tr>
                     <tr>
                         <td>Password:</td>
-                        <td><input type="password" name="password" id="password" required/></td>
+                        <td><input 
+                            className="form__input"
+                            type="password" 
+                            name="password" 
+                            id="password" 
+                            required
+                        /></td>
                     </tr>
                     <tr>
                         <td>Confirm Password:</td>
-                        <td><input type="password" name="confirmPassword" id="confirmPassword" required/></td>
+                        <td><input 
+                            className="form__input"
+                            type="password" 
+                            name="confirmPassword" 
+                            id="confirmPassword" 
+                            required
+                        /></td>
                     </tr>
                 </table>
                 <input type="submit" value="Submit" />
