60d3a6d829983a9396492f6cd9e541e966331294
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-10 21:33:52 -0500

    fixed non displaying profile pic for review

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import '../style/review.css';
 import Ratings from './Ratings';
 import Avatar from '@mui/material/Avatar';
+import axios from 'axios';
 
 export default function Reviews({review}) {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get(`http://localhost:3003/users/${review.author_id}`)
+        .then((res) => setUser(res.data))
+        .catch((e) => console.warn("catch", e))
+    },[review.author_id])
+    //console.log(user[0].image);
     return (
         <div className='review__container'>
             <div className='review__container__profile__pic'>
-                <Avatar src='' alt='pic' sx={{ width: 30, height: 30 }} />
+                <Avatar src={user.image} alt='pic' sx={{ width: 30, height: 30 }} />
                 <Ratings rating={review.rating}/>
             </div>
             <div className='pt-3'>{review.title}</div>
