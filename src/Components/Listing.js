931ea5877014cdee018bce4be0248924f1f06c62
crsvalle <cristian.valle@pursuit.org>
2023-11-12 12:03:36 -0500

    updated listings page, changed into better cards

--- src/Components/Listing.js
+++ src/Components/Listing.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Tooltip
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -15,26 +25,72 @@ export default function Listing({listing}) {
     }).catch((e) => console.error("catch", e))
 }, [listing.host]);
 
-  return (
-    <div>
-  <a
-    href={`/listings/${listing.listing_id}`}
-    className="flex flex-col my-1 bg-gray-800 border border-gray-200 rounded-lg shadow max-w-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700  dark:hover:bg-gray-700"
-  >
-    <div className="w-full aspect-w-3 aspect-h-4 md:w-48">
+
+
+return (
+  <Card className="w-full max-w-[30rem] shadow-lg my-1">
+    <CardHeader floated={false} color="blue-gray">
       <img
-        className="object-cover w-full h-full rounded-t-lg md:rounded-none md:rounded-l-lg"
         src="https://dingdong887180022.files.wordpress.com/2021/06/empty-room.jpg"
-        alt=""
+        alt="ui/ux review check"
       />
-    </div>
-    <div className="flex flex-col justify-between p-4 leading-normal">
-      <h5 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{listing.size}</h5>
-      <h6 className="mb-3 text-sm font-bold tracking-tight text-gray-900 dark:text-white">by: {host.firstname} {host.lastname}</h6>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Type: {listing.type} </p>
-      <p className="mb-2 font-normal text-gray-50 ">Price: ${listing.price} per month</p>
-    </div>
-  </a>
-</div>
-  )
+      <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+    </CardHeader>
+    <CardBody>
+      <div className="mb-3 flex items-center justify-between">
+        <Typography variant="h5" color="blue-gray" className="font-medium">
+          {listing.size}
+        </Typography>
+        <Typography
+            color="blue-gray"
+            className="flex items-center gap-1.5 font-normal"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="-mt-0.5 h-5 w-5 text-yellow-700"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                clipRule="evenodd"
+              />
+            </svg>
+            {listing.avg_rating.toFixed(1)}
+          </Typography>
+      </div>
+      <Typography color="gray">
+        Hosted by: {host.firstname} {host.lastname}
+      </Typography>
+      <div className="group mt-8 inline-flex flex-wrap items-center gap-3 ">
+        <p style={{color:"green"}}>${listing.price}.00 per month</p>
+        <Tooltip content="24/7 Availability">
+            <span className="cursor-pointer rounded-full border border-gray-900/5 bg-gray-900/5 p-3 text-gray-900 transition-colors hover:border-gray-900/10 hover:bg-gray-900/10 hover:!opacity-100 group-hover:opacity-70">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 2C9.79 2 8 3.79 8 6s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 7c-2.67 0-8 1.34-8 4v3c0 .55.45 1 1 1h14c.55 0 1-.45 1-1v-3c0-2.66-5.33-4-8-4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </Tooltip>
+      </div>
+    </CardBody>
+    <CardFooter className="pt-3">
+      <Link to={`/listings/${listing.listing_id}`}>
+      <Button size="lg" fullWidth={true}>
+        See more
+      </Button>
+      </Link>
+    </CardFooter>
+  </Card>
+);
+
 }
--- src/Components/Listings.js
+++ src/Components/Listings.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import Listing from "./Listing";
 import axios from "axios";
 
+import "../style/listings.css"
+
 const API = process.env.REACT_APP_API_URL;
 
 export default function Listings() {
@@ -15,9 +17,7 @@ export default function Listings() {
         }).catch((e) => console.error("catch", e))
     }, []);
 
-    console.log(listings)
   return (
-    //this belongs in the component listing.
     <div className="listing-card">
         {listings.map((listing)=>{
             return <Listing key={listing.id} listing={listing} />
