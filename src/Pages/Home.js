6f0f609774f19ef108feac81ff473814cbab00f4
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-12 16:14:37 -0500

    fixed typo

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import Lists from "../Components/Lists"
 import '../style/Home.css'
 import hero from './house.png'
 import ListingReviews from "./ListingReviews";
-import Geolocation from "../Components/GeoLocation";
+import GeoLocation from "../Components/GeoLocation";
 
 
 
@@ -14,7 +14,7 @@ export default function Home() {
       <Searchbar className="search__bar"/>
       <Lists/>
       <ListingReviews/>
-      <Geolocation/>
+      <GeoLocation/>
     </div>
   )
 }
