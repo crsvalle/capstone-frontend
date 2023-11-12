5b39cf20392f0f4650a4eb8d33d8386b9e239f39
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-12 15:53:49 -0500

    added component to get user geo location

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,30 @@
-export default function Map() {
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+};
+
+const center = {
+  lat: 0, // Set initial latitude
+  lng: 0, // Set initial longitude
+};
+
+const mapAPI = process.env.GEOLOCATION_API
+
+const Map = ({ location }) => {
   return (
-    <div>Map</div>
-  )
-}
+    <LoadScript googleMapsApiKey='AIzaSyD2SLQIAqwaYIHmbFmM1_bC7mkAK2FkRZ4'>
+      <GoogleMap 
+        mapContainerStyle={containerStyle} 
+        center={location}
+        zoom={13}
+      >
+        <Marker position={location} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Map;
