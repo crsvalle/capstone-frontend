1ee5d6286cf3306eb65ae995da0efbe6ad38d433
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-13 20:25:12 -0500

    npm installed

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,27 +1,76 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, MarkerClusterer, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '400px',
   height: '400px',
 };
 
-const center = {
-  lat: 0,
-  lng: 0,
-};
-
-const mapAPI = process.env.GEOLOCATION_API
+const MAP_API = process.env.GEOLOCATION_API;
 
 const Map = ({ location }) => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get('http://localhost:3003/listings')
+      .then((res) => setList(res.data))
+      .catch((e) => console.warn("catch", e))
+      .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    const fetchGeocodedMarkers = async () => {
+      const newMarkers = [];
+
+      for (const el of list) {
+        try {
+          const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+              address: el.address,
+              key: MAP_API,
+            },
+          });
+
+          const results = response.data.results;
+
+          if (results.length > 0) {
+            newMarkers.push({
+              position: results[0].geometry.location,
+            });
+          }
+        } catch (error) {
+          console.error('Geocoding error:', error);
+        }
+      }
+
+      setMarkers(newMarkers);
+    };
+
+    fetchGeocodedMarkers();
+  }, [list]);
+
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
-    <LoadScript googleMapsApiKey={mapAPI}>
-      <GoogleMap 
-        mapContainerStyle={containerStyle} 
+    <LoadScript googleMapsApiKey={MAP_API}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
         center={location}
         zoom={11}
       >
-        <Marker position={location} />
+        <MarkerClusterer>
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker.position} />
+          ))}
+        </MarkerClusterer>
       </GoogleMap>
     </LoadScript>
   );
