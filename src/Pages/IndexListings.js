d6195b745e5f54e7db893a7e98344fdb3e61bcef
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-19 15:08:58 -0500

    added search component in div tag with class name

--- src/Pages/IndexListings.js
+++ src/Pages/IndexListings.js
@@ -1,16 +1,21 @@
 import Listings from "../Components/Listings";
 import GeoLocation from "../Components/GeoLocation";
+import Searchbar from "../Components/Searchbar";
 import '../style/IndexListings.css'
+
 
 export default function IndexListings() {
   return (
+    <div>
     <div className="whole-page">
       <section className="left">
         <Listings/>
       </section>
       <section className="right">
+        <div className="indexListings__searchbar"><Searchbar /></div>
         <GeoLocation/>
       </section>
+    </div>
     </div>
   )
 }
