e02e80c46f908e78645dbc881fa3abe4c21ab05c
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-27 19:33:28 -0500

    added state for hover for listings

--- src/Components/Listings.js
+++ src/Components/Listings.js
@@ -1,29 +1,47 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Listing from "./Listing";
 import axios from "axios";
+import GeoLocation from "../Components/GeoLocation";
 
-import "../style/listings.css"
+import "../style/listings.css";
 
 const API = process.env.REACT_APP_API_URL;
 
 export default function Listings() {
-    const [listings, setListings] = useState([]);
+  const [listings, setListings] = useState([]);
+  const [hoveredListingId, setHoveredListingId] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        axios
-        .get(`${API}/listings`)
-        .then((response) =>{
-            setListings((response.data));
-        }).catch((e) => console.error("catch", e))
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`${API}/listings`)
+      .then((response) => {
+        setListings(response.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.error("catch", e);
+        setLoading(false);
+      });
+  }, []);
 
-    console.log(listings)
   return (
-    <div className="listing-card">
-        {listings.map((listing)=>{
-            return <Listing key={listing.listing_id} listing={listing} />
-        })}
-    </div>
-
-  )
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="listing-card">
+          {listings.map((listing) => (
+            <Listing
+              key={listing.listing_id}
+              listing={listing}
+              onMouseEnter={() => setHoveredListingId(listing.listing_id)}
+              onMouseLeave={() => setHoveredListingId(null)}
+            />
+          ))}
+        </div>
+      )}
+      <GeoLocation hoveredListingId={hoveredListingId} />
+    </>
+  );
 }
