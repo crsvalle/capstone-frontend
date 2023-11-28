be1bb2345e12c07b7ccc5a91d486387de5fea362
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-27 21:55:22 -0500

    added pic to home page with some text

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,8 @@ import Searchbar from "../Components/Searchbar";
 import Lists from "../Components/Lists"
 import '../style/Home.css'
 import hero from './house.png'
+import china from './Pic/china.png'
+import packing from './Pic/packing_boxes.png'
 import ListingReviews from "./ListingReviews";
 
 
@@ -11,6 +13,16 @@ export default function Home() {
     <div className="home">
       <img src={hero}></img>
       <div className="search__bar" ><Searchbar /></div>
+      <div className="home__img">
+        <div className="home__img__wrapper">
+          <div className="home__img__text1">"Your Memories, Our Keepsake.<br/>Where Memories Find Their Home.<br/>Making Space a Keepsake Experience."</div>
+          <img className="home__img__china" src={china}></img>
+        </div>
+        <div className="home__img__wrapper">
+          <img className="home__img__packing" src={packing}></img>
+          <div className="home__img__text2">"Safe and Simple,<br/> Where space meets serenity." </div>
+        </div>
+      </div>
       <Lists/>
       <ListingReviews/>
     </div>
