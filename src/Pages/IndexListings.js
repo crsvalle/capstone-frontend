b639053e383e90bf0416e6d453fba36e8e95ed1a
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-11-30 21:45:05 -0500

    added googlemapsloader and sent it as prpos to searchbar and listings component

--- src/Pages/IndexListings.js
+++ src/Pages/IndexListings.js
@@ -2,14 +2,22 @@ import Listings from "../Components/Listings";
 // import GeoLocation from "../Components/GeoLocation";
 import Searchbar from "../Components/Searchbar";
 import '../style/IndexListings.css'
+import { Loader } from '@googlemaps/js-api-loader';
 
 
+const apiKey = process.env.REACT_APP_GEOLOCATION_API;
+
+const googleMapsLoader = new Loader({
+  apiKey: apiKey,
+  version: 'weekly',
+  libraries: ['places'],
+});
 export default function IndexListings() {
   return (
     <div>
-      <div className="indexListings__searchbar"><Searchbar /></div>
+      <div className="indexListings__searchbar"><Searchbar googleMapsLoader={googleMapsLoader}/></div>
       <div className="whole-page">
-          <Listings/>
+          <Listings googleMapsLoader={googleMapsLoader}/>
           {/* <GeoLocation/> */}
       </div>
     </div>
