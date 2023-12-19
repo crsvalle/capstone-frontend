67b12510e03e5b230d60fa793f6bf80aba5f11f2
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-12-19 10:43:47 -0500

    encapsulated the whole app component inside stripe element

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from "react-router-dom";
 import { useSelector } from 'react-redux'
 
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
 //PAGES
 import Home from "./Pages/Home";
 import FourOFour from "./Pages/FourOFour";
@@ -36,41 +39,45 @@ const RestrictedRoutes = () =>{
 
   return <>{!isAuth ? <Outlet /> : <Navigate to={'/user/profile'} />}</>
 }
+// const KEY = process.env.REACT_APP_STRIPE_API_KEY
+const stripePromise = loadStripe('pk_test_51O9Gs7Cc76lpQQjkc6I6oeSL3jvYpPB3oxLFVtZkyl2yUr3kcaCALKCRd4XMutSePlqF4C3s5CyB2zFhV0TIvnEl00n6xcO1e3');
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />} />
+    <Elements stripe={stripePromise}>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
 
-          <Route element={<PrivateRoutes />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/listings/new" element={<NewListing/>}/>
-            <Route path="/listings/:id/edit" element={<EditListing />} />
-          </Route>
+            <Route element={<PrivateRoutes />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/listings/new" element={<NewListing/>}/>
+              <Route path="/listings/:id/edit" element={<EditListing />} />
+            </Route>
 
-          <Route element={<RestrictedRoutes />}>
-            <Route path="/register" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-          </Route>
-        
-          <Route path="/listings/:index" element={<IndexListings />} />
-          {/* <Route path="/listings/new" element={<NewListing/>}/> */}
-          <Route path="/listings/show/:index" element={<ShowListing />}/>
-          {/* <Route path="/listings/:index/edit" element={<EditListing />} /> */}
-          <Route path="/confirmation/" element={ <Confirmation />}  />
-          <Route path="/user/profile" element={<User />} />
-          <Route path="/user/:index/edit" element={<EditUser/>} />
-          <Route path="/register" element={<SignUp/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/checkout" element={<Checkout/>} />
-          <Route path='*' element={<FourOFour />}/>
-        </Routes>
-      </main>
-      <Footer/>
-    </Router>
+            <Route element={<RestrictedRoutes />}>
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+            </Route>
+          
+            <Route path="/listings/:index" element={<IndexListings />} />
+            {/* <Route path="/listings/new" element={<NewListing/>}/> */}
+            <Route path="/listings/show/:index" element={<ShowListing />}/>
+            {/* <Route path="/listings/:index/edit" element={<EditListing />} /> */}
+            <Route path="/confirmation" element={ <Confirmation />}  />
+            <Route path="/user/profile" element={<User />} />
+            <Route path="/user/:index/edit" element={<EditUser/>} />
+            <Route path="/register" element={<SignUp/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/checkout" element={<Checkout/>} />
+            <Route path='*' element={<FourOFour />}/>
+          </Routes>
+        </main>
+        <Footer/>
+      </Router>
+    </Elements>
   );
 }
 
