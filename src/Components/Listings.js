e3789390c10296bcf7fb884a0795b70921c991c0
crsvalle <cristian.valle@pursuit.org>
2023-12-31 23:37:38 -0500

    map is responsive

--- src/Components/Listings.js
+++ src/Components/Listings.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import GeoLocation from "../Components/GeoLocation";
 
 import "../style/listings.css";
+
 import { useParams } from "react-router-dom";
 
 const API = process.env.REACT_APP_API_URL;
@@ -19,7 +20,6 @@ export default function Listings({ googleMapsLoader }) {
     .get(`${API}/listings/search?query=${index}`)  //querry search backend, the index is passed as zip from searchbar
       .then((response) => {
         setListings(response.data);
-        //console.log(response.data);
         setLoading(false);
       })
       .catch((e) => {
@@ -29,22 +29,22 @@ export default function Listings({ googleMapsLoader }) {
   }, [index]);
 
   return (
-    <>
-      {loading ? (
-        <div className="loading-spinner">Loading...</div>
-      ) : (
-        <div className="listing-card">
-          {listings.map((listing, index) => (
-            <Listing
-              key={index}
-              listing={listing}
-              onMouseEnter={() => setHoveredListingId(listing.listing_id)}
-              onMouseLeave={() => setHoveredListingId(null)}
-            />
-          ))}
-        </div>
-      )}
-      <GeoLocation listing={listings} hoveredListingId={hoveredListingId} googleMapsLoader={googleMapsLoader} />
-    </>
+        <>
+          {loading ? (
+            <div className="loading-spinner">Loading...</div>
+          ) : (
+            <div className="listing-card">
+              {listings.map((listing, index) => (
+                <Listing
+                  key={index}
+                  listing={listing}
+                  onMouseEnter={() => setHoveredListingId(listing.listing_id)}
+                  onMouseLeave={() => setHoveredListingId(null)}
+                />
+              ))}
+            </div>
+          )}
+          {/* <GeoLocation listing={listings} hoveredListingId={hoveredListingId} googleMapsLoader={googleMapsLoader} /> */}
+        </>
   );
 }
