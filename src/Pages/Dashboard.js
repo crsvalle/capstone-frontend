2c0a5704d8dac32956a222182c438206c28c50ef
crsvalle <cristian.valle@pursuit.org>
2024-09-09 15:43:47 -0400

    added dashboard template

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,5 +1,18 @@
+import '../style/dashboard.css'
+
+
+//required things to do, in api create a way to check listings, current and past
+// do we have a way to see listings that are up and ones that were removed?
+//both current and past listings can share the same card
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='dashboardPage'>
+      <section className='curr-listings'>
+        <p>Current listings</p>
+      </section>
+      <section className='past-Listings'>
+        <h1>Past Listings</h1>
+      </section>
+    </div>
   )
 }
